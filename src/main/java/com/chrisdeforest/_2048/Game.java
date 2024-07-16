200406337c875b67117fea3a1dd549d818652a54
Chris DeForest <ced6563@rit.edu>
2024-07-15 20:42:18 -0400

    Commented code

--- src/main/java/com/chrisdeforest/_2048/Game.java
+++ src/main/java/com/chrisdeforest/_2048/Game.java
@@ -1,3 +1,9 @@
+/**
+ * Game.java
+ * This class represents the main game logic for the 2048 game.
+ * It handles the game state, tile generation, board manipulation,
+ * and game-over/win conditions.
+ */
 package com.chrisdeforest._2048;
 
 import java.beans.PropertyChangeListener;
@@ -6,19 +12,32 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The Game class encapsulates the state and logic of a 2048 game.
+ * It manages the board, scores, and game state (e.g., whether the game
+ * has been won or is over). It also supports property change listeners
+ * for updating the UI or other components when the game state changes.
+ */
 public class Game {
     /**
+     *  Debug flag for custom tile generation.
      *  ONLY CHANGE DEBUG IF YOU KNOW WHAT YOU ARE DOING
-     *  See generateTile() method for options
+     *  See generateTile() method for more options
      */
     private boolean debug = false;
 
-    public final static int BOARD_SIZE = 4, WINNING_SCORE = 2048;
-    private final PropertyChangeSupport support;
-    private final Tile[][] board;
-    private final Random rand;
+    public final static int BOARD_SIZE = 4; // The size of the game board (4x4)
+    private final static int WINNING_SCORE = 2048; // The score needed to winn the game
+    private final PropertyChangeSupport support; // Property change support for notifying listeners of changes
+    private final Tile[][] board; // The game board, represented as a 2D array of Tiles
+    private final Random rand; // Random number generator for tile placement
+    // Game state variables
     private boolean gameWon, continued, gameOver, sameBoard;
     private int oldScore, newScore, bestScore, moveCount;
+
+    /**
+     * Default constructor. Initializes a new game with an empty board.
+     */
     public Game(){
         this.board = new Tile[BOARD_SIZE][BOARD_SIZE];
         this.rand = new Random();
@@ -30,6 +49,12 @@ public class Game {
         this.moveCount = 0;
         initializeBoard();
     }
+
+    /**
+     * Copy constructor. Initializes a new game with the state of the given game.
+     *
+     * @param game The game to copy the state from
+     */
     public Game(Game game){
         this.board = new Tile[BOARD_SIZE][BOARD_SIZE];
         this.rand = new Random();
@@ -50,6 +75,10 @@ public class Game {
             }
         }
     }
+
+    /**
+     * Initializes the game board with empty tiles.
+     */
     public void initializeBoard(){
         for(int i = 0; i < BOARD_SIZE; i++){
             for(int j = 0; j < BOARD_SIZE; j++){
@@ -57,6 +86,10 @@ public class Game {
             }
         }
     }
+
+    /**
+     * Clears the game board, resetting all tiles to their default values.
+     */
     public void clearBoard(){
         for(int i = 0; i < BOARD_SIZE; i++){
             for(int j = 0; j < BOARD_SIZE; j++){
@@ -66,6 +99,11 @@ public class Game {
             }
         }
     }
+
+    /**
+     * Starts a new game by clearing the board, resetting scores and game state,
+     * and generating the initial two tiles
+     */
     public void newGame(){
         clearBoard();
         this.oldScore = 0;
@@ -79,25 +117,30 @@ public class Game {
         generateTile(this.debug);
         this.support.firePropertyChange("newGame", null, rand.nextInt(10));
     }
+
+    /**
+     * Generates a new tile on the board. If debug mode is enabled,
+     * custom tiles are generated at specific positions.
+     *
+     * @param debug Flag indicating whether to generate custom tiles for debugging.
+     */
     public void generateTile(boolean debug){
-        /* enabling debug allows for the customization of where tiles spawn, their value, and their moveCount
-         * it also prints the board's values to the console
-         */
+        // Debug mode: Custom tile generation for testing
         if(debug){
-            int pos = 1, num = 2;
-            this.board[pos-1][pos].setValue(num*16);
-            this.board[pos-1][pos].setMoveGenerated(this.moveCount);
-            this.board[pos][pos].setValue(num*4);
+            int pos = 0, val = 2;
+            this.board[pos][pos].setValue(val*16);
             this.board[pos][pos].setMoveGenerated(this.moveCount);
-            this.board[pos+1][pos].setValue(num);
+            this.board[pos+1][pos].setValue(val*4);
             this.board[pos+1][pos].setMoveGenerated(this.moveCount);
-            this.board[pos+2][pos].setValue(num*4);
+            this.board[pos+2][pos].setValue(val);
             this.board[pos+2][pos].setMoveGenerated(this.moveCount);
+            this.board[pos+3][pos].setValue(val*4);
+            this.board[pos+3][pos].setMoveGenerated(this.moveCount);
             System.out.println(this);
-        } else { // default random tile generation
+        } else { // Default random tile generation
             if ((!gameWon || continued) && !gameOver) {
                 int t1 = ((rand.nextInt(1, 5) % 4) == 0) ? 4 : 2, r1 = rand.nextInt(4), c1 = rand.nextInt(4);
-                while (!board[r1][c1].isEmpty()) {
+                while (board[r1][c1].isNotEmpty()) {
                     r1 = rand.nextInt(4);
                     c1 = rand.nextInt(4);
                 }
@@ -108,6 +151,15 @@ public class Game {
         // check if the game is over after a tile generates
         checkForGameOver();
     }
+
+    /**
+     * Determines whether to generate a new tile based on the state of the board
+     * and the current move direction.
+     *
+     * @param sameBoard Flag indicating whether the board state is the same as before the move.
+     * @param iteration The current iteration of the move (0 for the initial move, 1 for final check).
+     * @param direction The direction of the move ("up", "down", "left", or "right").
+     */
     public void generateTileDecision(boolean sameBoard, int iteration, String direction){
         if(sameBoard && iteration == 0) {
             this.support.firePropertyChange(direction, -1, 0);
@@ -118,16 +170,23 @@ public class Game {
             this.support.firePropertyChange(direction, -10, 0);
         }
     }
+
+    /**
+     * Moves tiles vertically based on the specified direction.
+     *
+     * @param iteration The current iteration of the move (0 for the initial move, 1 for final check).
+     * @param direction The direction of the move ("up" or "down").
+     */
     public void moveVertical(int iteration, String direction) {
         sameBoard = true;
         for (int i = 0; i < BOARD_SIZE; i++) {
             Tile[] col = {board[0][i], board[1][i], board[2][i], board[3][i]};
             List<Tile> newCol = new LinkedList<>();
             for (Tile t : col) {
-                if (!t.isEmpty())
+                if (t.isNotEmpty())
                     newCol.add(t);
             }
-            // adds back the missing values once values have been moved around
+            // Adds back the missing values once values have been moved around
             int missingTiles = 4 - newCol.size();
             for (int j = 0; j < missingTiles; j++) {
                 if (direction.equals("up"))
@@ -136,13 +195,13 @@ public class Game {
                     newCol.addFirst(new Tile());
                 }
             }
-            // takes care of tile merging if tiles of the same value are moved up
+            // Handles tile merging if tiles of the same value are moved up or down
             if (iteration == 0)
                 newCol = condense(newCol, direction);
-            // updates the values in the Tile[][] board's current row to the new row values
+            // Updates the values in the Tile[][] board's current row to the new row values
             for (int j = 0; j < BOARD_SIZE; j++)
                 this.board[j][i] = newCol.get(j);
-            // checking to see whether a tile should be generated or not
+            // Checking to see whether a tile should be generated or not
             for (int j = 0; j < BOARD_SIZE; j++) {
                 if (!(((col[j].getValue() == newCol.get(j).getValue())))) {
                     sameBoard = false;
@@ -150,19 +209,26 @@ public class Game {
                 }
             }
         }
-        // sending an update whether a tile will be generated or not
+        // Sending an update whether a tile will be generated or not
         generateTileDecision(sameBoard, iteration, direction);
     }
+
+    /**
+     * Moves tiles horizontally based on the specified direction.
+     *
+     * @param iteration The current iteration of the move (0 for the initial move, 1 for final check).
+     * @param direction The direction of the move ("left" or "right").
+     */
     public void moveHorizontal(int iteration, String direction){
         sameBoard = true;
         for(int i = 0; i < BOARD_SIZE; i++) {
             Tile[] row = {board[i][0], board[i][1], board[i][2], board[i][3]};
             List<Tile> newRow = new LinkedList<>();
             for (Tile t : row) {
-                if (!t.isEmpty())
+                if (t.isNotEmpty())
                     newRow.add(t);
             }
-            // adds back the missing values once values have been moved around
+            // Adds back the missing values once values have been moved around
             int missingTiles = 4 - newRow.size();
             for (int j = 0; j < missingTiles; j++){
                 if(direction.equals("left"))
@@ -171,13 +237,13 @@ public class Game {
                     newRow.addFirst(new Tile());
                 }
             }
-            // takes care of tile merging if tiles of the same value are moved right
+            // Handles tile merging if tiles of the same values are moved right or left
             if(iteration == 0)
                 newRow = condense(newRow, direction);
-            // updates the values in the Tile[][] board's current row to the new row values
+            // Updates the values in the Tile[][] board's current row to the new row values
             for(int j = 0; j < BOARD_SIZE; j++)
                 this.board[i][j] = newRow.get(j);
-            // checking to see whether a tile should be generated or not
+            // Checking to see whether a tile should be generated or not
             for(int j = 0; j < BOARD_SIZE; j++) {
                 if (!(((row[j].getValue() == newRow.get(j).getValue())))) {
                     sameBoard = false;
@@ -185,9 +251,18 @@ public class Game {
                 }
             }
         }
-        // sending an update whether a tile will be generated or not
+        // Sending an update whether a tile will be generated or not
         generateTileDecision(sameBoard, iteration, direction);
     }
+
+    /**
+     * Condenses a list of tiles by merging adjacent tiles of the same value.
+     * Updates the score based on the merged values.
+     *
+     * @param list The list of tiles to condense
+     * @param direction The direction of the move ("up", "down", "left", "right").
+     * @return The condensed list of tiles
+     */
     public List<Tile> condense(List<Tile> list, String direction){
         if(direction.equals("up") || direction.equals("left")){
             for(int j = 0; j < BOARD_SIZE - 1; j++){
@@ -214,6 +289,10 @@ public class Game {
         checkForWin();
         return list;
     }
+
+    /**
+     * Checks if the game has been won (i.e., a tile with the winning score has been created).
+     */
     public void checkForWin(){
         for(int i = 0; i < BOARD_SIZE; i++){
             for(int j = 0; j < BOARD_SIZE; j++){
@@ -226,17 +305,21 @@ public class Game {
             }
         }
     }
+
+    /**
+     * Checks if the game is over (i.e., no more moves can be made).
+     */
     public void checkForGameOver(){
         int zeros = 0;
         boolean canMove = true;
-        // checking for zeroes
+        // Checking the board for zeroes
         for(int i = 0; i < BOARD_SIZE; i++){
             for(int j = 0; j < BOARD_SIZE; j++){
                 if(board[i][j].getValue() == 0)
                     zeros++;
             }
         }
-        // checking the board for possible moves if the board is full
+        // Checking the board for possible moves if the board is full
         if(zeros == 0) {
             Game newGame = new Game(this);
             newGame.moveVertical(0, "up");
@@ -253,49 +336,120 @@ public class Game {
                 }
             }
         }
-        // if there are no zero tiles left and no moves are possible, then the game is over
+        // If there are no zero tiles left and no moves are possible, then the game is over
         if(zeros == 0 && !canMove) {
             this.gameOver = true;
             this.support.firePropertyChange("game over", null, 0);
         }
     }
+
+    /**
+     * Continues the game after reaching the winning score.
+     */
     public void continueGame(){
         this.continued = true;
         this.support.firePropertyChange("continue", null, 0);
     }
+
+    // Getter and Setter methods
+
+    /**
+     * Returns the current game board.
+     *
+     * @return The game board.
+     */
     public Tile[][] getBoard(){
         return this.board;
     }
+
+    /**
+     * Returns whether the game has been won.
+     *
+     * @return True if the game has been won, false otherwise.
+     */
     public boolean getGameWon(){
         return this.gameWon;
     }
+
+    /**
+     * Returns the old score (before the last move).
+     *
+     * @return The old score.
+     */
     public int getOldScore(){
         return this.oldScore;
     }
+
+    /**
+     * Sets the old score (before the last move).
+     *
+     * @param s The old score.
+     */
     public void setOldScore(int s){
         this.oldScore = s;
     }
+
+    /**
+     * Returns the new score (after the last move).
+     *
+     * @return The new score.
+     */
     public int getNewScore(){
         return this.newScore;
     }
+
+    /**
+     * Returns the best score achieved in the game.
+     *
+     * @return The best score.
+     */
     public int getBestScore(){
         return this.bestScore;
     }
+
+    /**
+     * Sets the best score achieved in the game.
+     *
+     * @param bs The best score.
+     */
     public void setBestScore(int bs){
         this.bestScore = bs;
     }
+
+    /**
+     * Returns whether the board state is the same as before the last move.
+     *
+     * @return True if the board state is the same, otherwise false.
+     */
     public boolean getSameBoard(){
         return this.sameBoard;
     }
+
+    /**
+     * Returns the current move count.
+     *
+     * @return The move count.
+     */
     public int getMoveCount(){
         return this.moveCount;
     }
+
+    /**
+     * Increments the move count by 1.
+     */
     public void incrementMoveCount(){
         this.moveCount++;
     }
+
+    /**
+     * Returns the debug flag.
+     *
+     * @return True if debug mode is enabled, otherwise false.
+     */
     public boolean getDebug(){
         return this.debug;
     }
+
     @Override
     public String toString(){
         StringBuilder string = new StringBuilder();
@@ -310,6 +464,7 @@ public class Game {
         }
         return string.toString();
     }
+
     @Override
     public boolean equals(Object object){
         if(object instanceof Game game) {
@@ -322,6 +477,12 @@ public class Game {
         }
         return true;
     }
+
+    /**
+     * Adds a property change listener to the game.
+     *
+     * @param listener The property change listener to add.
+     */
     public void addPropertyChangeListener(PropertyChangeListener listener){
         this.support.addPropertyChangeListener(listener);
     }
--- src/main/java/com/chrisdeforest/_2048/Tile.java
+++ src/main/java/com/chrisdeforest/_2048/Tile.java
@@ -1,42 +1,113 @@
+/**
+ * Tile.java
+ * This class represents a tile in the 2048 game. Each tile has a value, a background color,
+ * a text color, and a font size based on its value. It also tracks whether an animation
+ * has been played and the move in which it was generated.
+ */
 package com.chrisdeforest._2048;
 
+/**
+ * The Tile class represents a tile in the 2048 game.
+ * It handles the tile's value, appearance, and state.
+ */
 public class Tile {
+    // Color codes for tiles based on their values
     private static final String COLOR_0 = "rgb(204, 192, 179)", COLOR_2 = "rgb(238, 228, 218)",
             COLOR_4 = "rgb(237, 224, 200)", COLOR_8 = "rgb(242, 177, 121)", COLOR_16 = "rgb(245, 149, 99)",
             COLOR_32 = "rgb(246, 124, 95)", COLOR_64 = "rgb(246, 94, 59)", COLOR_128 = "rgb(237, 207, 114)",
             COLOR_256 = "rgb(237, 204, 97)", COLOR_512 = "rgb(237, 200, 80)", COLOR_1024 = "rgb(237, 197, 63)",
             COLOR_2048 = "rgb(237, 194, 46)", COLOR_DEFAULT = "rgb(0, 0, 0)";
-    private int value, moveGenerated;
-    private boolean animationPlayed;
+    private int value; // The value of the tile; default: 0
+    private int moveGenerated; // The move number when the tile was generated; default: -1
+    private boolean animationPlayed; // Whether the animation for this tile has been played
+
+    /**
+     * Default constructor. Initializes a Tile with a value of 0, generated on move -1, and
+     * has not yet played an animation
+     */
     public Tile() {
         this.value = 0;
         this.moveGenerated = -1;
         this.animationPlayed = false;
     }
+
+    /**
+     * Constructor that initializes the tile with a specified value
+     *
+     * @param v The value of the tile
+     */
     public Tile(int v){
         this.value = v;
     }
+
+    /**
+     * Gets the value of the tile
+     *
+     * @return The value of the tile
+     */
     public int getValue(){
         return this.value;
     }
+
+    /**
+     * Sets the value of the tile
+     *
+     * @param v The new value of the tile
+     */
     public void setValue(int v){
         this.value = v;
     }
+
+    /**
+     * Gets the move number when the tile was generated
+     *
+     * @return The move number when the tile was generated
+     */
     public int getMoveGenerated(){
         return this.moveGenerated;
     }
+
+    /**
+     * Sets the move number when the tile was generated
+     *
+     * @param mg The move number when the tile was generated
+     */
     public void setMoveGenerated(int mg){
         this.moveGenerated = mg;
     }
+
+    /**
+     * Checks if the animation for this tile has been played
+     *
+     * @return True if the animation has been played, false otherwise
+     */
     public boolean getAnimationPlayed(){
         return this.animationPlayed;
     }
+
+    /**
+     * Sets whether the animation for this tile has been played
+     *
+     * @param ap True if the animation has been played, false otherwise
+     */
     public void setAnimationPlayed(boolean ap){
         this.animationPlayed = ap;
     }
-    public boolean isEmpty(){
-        return this.value == 0;
+
+    /**
+     * Checks if the tile is empty (i.e., its value is 0).
+     *
+     * @return True if the tile is not empty, false otherwise
+     */
+    public boolean isNotEmpty(){
+        return this.value != 0;
     }
+
+    /**
+     * Gets the background color of the tile based on its value
+     *
+     * @return The background color of the tile
+     */
     public String getBackground() {
         return switch (this.value) {
             case 0 -> COLOR_0;
@@ -54,12 +125,24 @@ public class Tile {
             default -> COLOR_DEFAULT;
         };
     }
+
+    /**
+     * Gets the text color of the tile based on its value
+     *
+     * @return The text color of the tile
+     */
     public String getTextColor(){
         if(this.value < 16)
             return "rgb(119, 110, 101)";
         else
             return "rgb(249, 246, 242)";
     }
+
+    /**
+     * Gets the font size of the tile based on its value
+     *
+     * @return The font size of the tile
+     */
     public String getFontSize(){
         return switch (this.value){
             case 0, 2, 4, 8, 16, 32, 64, 128, 256, 512 -> "40pt";
